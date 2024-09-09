8bc4228023b280cab483d67cf9a53d04b8c3cf76
Floriane TUERNAL-SABOTINOV <ftuertinov@proton.me>
2024-09-09 16:49:36 +0200

    simple welcome page in html

--- django_fakestore/django_fakestore/fakestore/settings.py
+++ django_fakestore/django_fakestore/fakestore/settings.py
@@ -62,8 +62,8 @@ ROOT_URLCONF = 'fakestore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        
-        
+        'DIRS': [],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
